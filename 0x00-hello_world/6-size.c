f904028f79cd580c3484e8bdf5e7339547a8d336
Ankubito <prudent.nkubito@gmail.com>
2023-06-17 10:13:12 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-puts{"Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a)};
-puts{"Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b)};
-puts{"Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c)};
-puts{"Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d)};
-puts{"Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)};
+puts("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+puts("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+puts("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+puts("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+puts("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }

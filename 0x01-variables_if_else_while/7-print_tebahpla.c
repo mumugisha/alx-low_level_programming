d3468d495e14ab049abe89e712a0e103d723119f
Ankubito <prudent.nkubito@gmail.com>
2023-06-18 13:29:22 -0700

    print whether the number stored in the variable n is positive or negative

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in
+ * reverse
+ *followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; n--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}

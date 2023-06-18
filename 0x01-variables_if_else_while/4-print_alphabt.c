c08053ada17bb63709cf7c8666d9e04de694d598
Ankubito <prudent.nkubito@gmail.com>
2023-06-18 12:10:49 -0700

    program that prints the alphabet in lowercase, followed by a new line without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet without q and e
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

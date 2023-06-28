3f37a67f524bb704f46a25b3cd258a4d44d6f470
Ankubito <prudent.nkubito@gmail.com>
2023-06-28 02:13:26 -0700

    function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,37 +2,36 @@
 
 /**
  * _atoi - Convert a string to an integer
- * @s: the Pointer to convert.
+ * @s: the Pointer to convert
  *
- * Return: Pointer to the destination string.
+ * Return: integer
  */
 int _atoi(char  *s)
 {
-int c = 0;
-		unsigned int ni = 0;
-		int min = 1;
-		int isi = 0;
+	int c = 0;
 
-		while (s[c])
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[c])
 	{
 		if (s[c] == 45)
-	{
-
-			 min * = -1;
-	}
-	while (s[c] >= 48 &&  s[c] <= 57)
+		{
+			min *= -1;
+		}
+		while (s[c] >= 48 &&  s[c] <= 57)
 	{
 		isi = 1;
 		ni = (ni * 10) + (s[c] - '0');
 		c++;
 	}
-	if (isi == 1)
+	if (isi ==  1)
 	{
 		break;
-		}
-	c++;
-}
-ni *= min;
-
-		return  (ni);
+	}
+		c++;
+	}
+	ni *=  min;
+	return  (ni);
 }

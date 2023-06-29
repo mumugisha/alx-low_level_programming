7e3e242b7966dbb982349c294f203981f1f92d00
Ankubito <prudent.nkubito@gmail.com>
2023-06-29 15:32:47 -0700

    function that reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array -  reverses the content
+ * of an array of integers
+ * @a: array
+ * @n: elements of arrays
+ *
+ * Return: (void)
+ */
+void reverse_array(int *a, int n)
+{
+				int b;
+				int d;
+
+					for (b = 0; b < n--; b++)
+					{
+						d = a[b];
+				a[b] = a[n]
+				a[n] = d
+
+		}
+}

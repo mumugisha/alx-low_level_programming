52034a779d6d923ff9534fdcb60ec8121edc9742
Ankubito <prudent.nkubito@gmail.com>
2023-06-29 20:31:06 -0700

    unction that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
+ * print_number -prints an integer
  * @n: integer
  *
  * Return: 0
@@ -9,7 +9,6 @@
 void print_number(int n)
 {
 	unsigned int n1;
-
 	n1 = n;
 
 	if (n < 0)
@@ -18,10 +17,11 @@ void print_number(int n)
 		n1 = -n;
 	}
 
-	if (n1 / 10 != 0)
+	if (n1/10 != 0)
 	{
-		print_number(n1 / 10);
+		print_number(n1/10);
 	}
 
-	_putchar((n1 % 10) + '0')
+	_putchar((n1 % 10) + '0');
+
 }

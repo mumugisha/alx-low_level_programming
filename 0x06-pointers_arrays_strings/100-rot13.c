5db69ef81912244be826ea78e1533e62555e5531
Ankubito <prudent.nkubito@gmail.com>
2023-07-01 02:44:59 -0700

    function that encodes a string using

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 
 /**
- * rot13 - Encodes a string using the ROT13 cipher
+ * rot13 - Encode rot13
  * @s: Pointer to the input string
  *
  * Return: *s
  */
 char *rot13(char *s)
 {
-	int i; 
+	int i;
 	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	
-	for (j = 0; j < 52; j++)
+
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
 			s[i] = datarot[j];
-							break;
+			break;
 				}
 			}
 		}

1fa4276caaf37bece54a35b799e78d7dd3dca4c7
Ankubito <prudent.nkubito@gmail.com>
2023-07-05 16:27:00 -0700

    function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a
+ * character in a string
+ * @s: input
+ * @c: input
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+		int a;
+
+		for (; s[a] = '\0'; a++)
+		{
+		if (s[i] == c)
+		return (&s[a]);
+		}
+
+		return (0);
+}

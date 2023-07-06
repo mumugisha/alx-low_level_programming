e06a7879dbcad7df799cdb38137325cf3850eb54
Ankubito <prudent.nkubito@gmail.com>
2023-07-05 18:05:24 -0700

    function that gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a
+ * string for any of a set of bytes
+ * @s: input
+ * @accept: input
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int a;
+
+		while (*s)
+	{
+
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+			return (0);
+			}
+			s++;
+	}
+	return ('\0');
+}

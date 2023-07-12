755408392db470942b84e5b2983500cadf9d0857
Ankubito <prudent.nkubito@gmail.com>
2023-07-12 11:38:35 -0700

    function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,6 @@
  * @s1: input 1 to concat
  * @s2: input 2 to concat
  * Return: concat s1 and s2
- *
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -21,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 		i = ci = 0;
 		while (s1[i] != '\0')
 			i++;
-		while (s2[i] != '\0')
+		while (s2[ci] != '\0')
 			ci++;
 
 		conct = malloc(sizeof(char) * (i + ci + 1));
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,46 +2,46 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to 
+ * alloc_grid - returns a pointer to
  * a 2 dimensional array of integers
  * @width: width input
- * Height: height input
+ * height: height input
  * return: pointer of 2 dim array
  */
 int **alloc_grid(int width, int height)
 {
 	int **haa;
-	int x,y;
-	
+	int x, y;
+
 	if (width <= 0 || height <= 0)
 	return (NULL);
-	
-	haa =malloc(sizeof(int *) * height);
-	
-	if haa == NULL)
-		return (NULL);;
-	
+
+	haa = malloc(sizeof(int *) * height);
+
+		if (haa == NULL)
+		return (NULL);
+
 	for (x = 0; x < height; x++)
 	{
 		haa[x] = malloc(sizeof(int) * width);
-	
+
 		if (haa[x] == NULL)
 		{
-			for (;x >= 0; x--)
+			for (; x >= 0; x--)
 				free(haa[]);
-			
+
 			free(haa);
 			return (NULL);
 		}
-		
+
 	}
-	
-	for(x = ; x < height; x++)
+
+	for (x = ; x < height; x++)
 	{
-		for (y = ; y< width; y++)
-		haa[x][y] = ;
-		
+		for (y = ; y < width; y++)
+		haa[x][y] = 0;
+
 	}
-	
+
 	return (haa);
 }

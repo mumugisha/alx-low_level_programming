76b2b8a9b2835982ecc1088ae41a680f69e08bb2
Ankubito <prudent.nkubito@gmail.com>
2023-07-12 19:50:42 -0700

    function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-main.c
+++ 0x0B-malloc_free/100-main.c
@@ -4,19 +4,20 @@
 
 /**
  * main - check the code for ALX School students.
- *
+ * @ac: argument count
+ * @av:argument vector
  * Return: Always 0.
  */
 int main(int ac, char *av[])
 {
-    char *s;
+		char *s;
 
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
+		s = argstostr(ac, av);
+		if (s == NULL)
+		{
+			return (1);
+		}
+		printf("%s", s);
+		free(s);
+		return (0);
 }

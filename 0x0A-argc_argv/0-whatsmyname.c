f4bf9a0ae2b0f82348536bb4e53b58958386fafe
Ankubito <prudent.nkubito@gmail.com>
2023-07-15 02:15:22 -0700

    program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints its name
+ * @argc: n arguments
+ * @argv: arrays arguments
+ *
+ * return: Always 0 (success)
+ *
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+1. Silence is argument carried out by other means

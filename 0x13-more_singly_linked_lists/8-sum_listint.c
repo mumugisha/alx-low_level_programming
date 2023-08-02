0822b7fb3f0e3adbb7d6bd4a7b73c0c38c3f2dab
Ankubito <prudent.nkubito@gmail.com>
2023-08-01 17:52:08 -0700

    function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,18 +4,18 @@
  * sum_listint - calculate sum of data in listint
  * @head: 1st node in linked ls
  *
- * Return: sum
+ * Return: result sum
  */
 int sum_listint(listint_t *head)
 {
-	int sum = 0;
+	int a = 0;
 	listint_t *temp = head;
 
 	while (temp)
 	{
-		sum += temp->n;
+		a += temp->n;
 		temp = temp->next;
 	}
 
-	return (sum);
+	return (a);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,9 +3,11 @@
 /**
  * insert_nodeint_at_index - add another node in linked ls
  * @head: pointer to 1st node ls
- * @index: index, where another node is added
+ * @idx: index, where another node is added
  * @n: data to add a node
+ * Return: pointer to node, or NULL
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int a;
@@ -19,16 +21,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (index == 0)
+	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 		return (new);
 	}
 
-	for (a = 0; temp && a < index; a++)
+	for (a = 0; temp && a < idx; a++)
 	{
-		if (a = index - 1)
+		if (a == idx - 1)
 		{
 			new->next = temp->next;
 			temp->next = new;

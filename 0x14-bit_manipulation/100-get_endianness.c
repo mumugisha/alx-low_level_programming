e30b44cd8dc9dc4877d6a36959dc7c58fb4afc50
Alphonse Nkubito <79317323+Ankubito@users.noreply.github.com>
2023-12-15 03:51:07 +0200

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_endianness - check if machine is small or big
+ * get_endianness - check if machine is small or bigger
  * Return: 0 if big, & 1 is small
  */
 int get_endianness(void)

a27b529cd72070dc19c4574a8d6334062750df7a
Alphonse <prudent.nkubito@gmail.com>
2024-03-25 18:26:16 -0700

    task 0

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * div - div two parts integers
+ * @a: first integer
+ * @b: second integer
+ * Return: divide num
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sub - subtact two parts integers
+ * @a: first integer
+ * @b: second integer
+ * Return: subtracted numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}

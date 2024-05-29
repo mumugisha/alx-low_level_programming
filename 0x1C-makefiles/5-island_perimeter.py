1de5282d7f413a35a568f0fd6441b0d4410b0770
Ankubito <prudent.nkubito@gmail.com>
2024-05-29 13:34:21 -0700

    make files task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-"""the perimeter of the island"""
-
 def island_perimeter(grid):
     """Return the perimeter of an island.
 
@@ -15,14 +12,14 @@ def island_perimeter(grid):
     width = len(grid[0])
     height = len(grid)
     edges = 0
-    size = 0
+    scope = 0
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-                if j > 0 and grid[i][j - 1] == 1:
+    for a in range(height):
+         for b in range(width):
+            if grid[a][b] == 1:
+                scope += 1
+                if b > 0 and grid[a][b - 1] == 1:
                     edges += 1
-                if i > 0 and grid[i - 1][j] == 1:
+                if a > 0 and grid[a - 1][b] == 1:
                     edges += 1
-    return size * 4 - edges * 2
+    return scope * 4 - edges * 2

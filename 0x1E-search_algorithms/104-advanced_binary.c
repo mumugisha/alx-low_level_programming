9bb893b3139ae4b31d4df2055de60870b80bce15
mumugisha <prudent.nkubito@gmail.com>
2024-07-17 16:38:16 -0700

    Search Algorithm

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -7,46 +7,46 @@
  */
 void printing_array(int *array, size_t size)
 {
-	size_t i;
+    size_t i;
 
-	printf("Searching in array");
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-	printf("\n");
+    printf("Searching in array");
+    for (i = 0; i < size; i++)
+        printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+    printf("\n");
 }
 
 /**
  * binary_search_index - searches for a value in a sorted array
  * using the binary search algorithm
- * @ar: Pointer to the first element of the array to search in
- * @lower: The index of the smallest in the array
- * @higher: The index of the highest in the array
+ * @array: Pointer to the first element of the array to search in
+ * @lower: The index of the smallest element in the array
+ * @higher: The index of the highest element in the array
  * @value: The value to search for
  *
  * Return: The first index where the value is located,
  *         or -1 if value is not present or array is NULL
  */
-int binary_search_index(int *ar, size_t lower, size_t higher, int value)
+int binary_search_index(int *array, size_t lower, size_t higher, int value)
 {
-	size_t mid;
+    size_t mid;
 
-	if (lower > higher)
-		return (-1);
+    if (lower > higher)
+        return (-1);
 
-	printing_array(ar + lower, higher - lower + 1);
+    printing_array(array + lower, higher - lower + 1);
 
-	mid = lower + (higher - lower) / 2;
+    mid = lower + (higher - lower) / 2;
 
-	if (ar[mid] == value)
-	{
-		if (mid == lower || ar[mid - 1] != value)
-			return (mid);
-		return (binary_search_index(ar, lower, mid, value));
-	}
-	else if (ar[mid] > value)
-		return (binary_search_index(ar, lower, mid - 1, value));
-	else
-		return (binary_search_index(ar, mid + 1, higher, value));
+    if (array[mid] == value)
+    {
+        if (mid == lower || array[mid - 1] != value)
+            return (mid);
+        return (binary_search_index(array, lower, mid, value));
+    }
+    else if (array[mid] > value)
+        return (binary_search_index(array, lower, mid - 1, value));
+    else
+        return (binary_search_index(array, mid + 1, higher, value));
 }
 
 /**
@@ -61,8 +61,8 @@ int binary_search_index(int *ar, size_t lower, size_t higher, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array == NULL)
-		return (-1);
+    if (array == NULL)
+        return (-1);
 
-	return (binary_search_index(array, 0, size - 1, value));
+    return (binary_search_index(array, 0, size - 1, value));
 }

ea7dd3d8e2d36eb04d14d52e67333a52502dc5dc
mumugisha <prudent.nkubito@gmail.com>
2024-07-17 11:28:47 -0700

    Search Algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,42 +12,43 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t pos, low, high;
-    double frac;
+	size_t pos, low, high;
+	double frac;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    low = 0;
-    high = size - 1;
+	low = 0;
+	high = size - 1;
 
-    while (size)
-    {
-        frac = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
-        pos = (size_t)(low + frac);
-        printf("Value checked array[%d]", (int)pos);
+	while (size)
+	{
+		frac = (double)(high - low) / (array[high] - array[low]) *
+		       (value - array[low]);
+		pos = (size_t)(low + frac);
+		printf("Value checked array[%d]", (int)pos);
 
-        if (pos >= size)
-        {
-            printf(" is out of range\n");
-            break;
-        }
-        else
-        {
-            printf(" = [%d]\n", array[pos]);
-        }
+		if (pos >= size)
+		{
+			printf(" is out of range\n");
+			break;
+		}
+		else
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
 
-        if (array[pos] == value)
-            return ((int)pos);
+		if (array[pos] == value)
+			return ((int)pos);
 
-        if (array[pos] < value)
-            low = pos + 1;
-        else
-            high = pos - 1;
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
 
-        if (low == high)
-            break;
-    }
+		if (low == high)
+			break;
+	}
 
-    return (-1);
+	return (-1);
 }

997f906888d0adfa64538068f9908f07adc48f44
mumugisha <prudent.nkubito@gmail.com>
2024-08-17 04:27:42 -0700

    File input and output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,25 +15,24 @@
  */
 char *allocate_buffer(char *file)
 {
-    char *buffer;
-    int i = 0;
+	char *buffer;
+	int i = 0;
 
-    buffer = malloc(sizeof(char) * BUF_SIZE);
+	buffer = malloc(sizeof(char) * BUF_SIZE);
 
-    if (buffer == NULL)
-    {
-        write(STDERR_FILENO, "Error: Can't write to ", 22);
-        
-        while (file[i] != '\0') /* Replacement for _strlen */
-        {
-            write(STDERR_FILENO, &file[i], 1);
-            i++;
-        }
-        write(STDERR_FILENO, "\n", 1);
-        exit(99);
-    }
+	if (buffer == NULL)
+	{
+		write(STDERR_FILENO, "Error: Can't write to ", 22);
+		while (file[i] != '\0') /* Replacement for _strlen */
+		{
+			write(STDERR_FILENO, &file[i], 1);
+			i++;
+		}
+		write(STDERR_FILENO, "\n", 1);
+		exit(99);
+	}
 
-    return (buffer);
+	return (buffer);
 }
 
 /**
@@ -42,41 +41,40 @@ char *allocate_buffer(char *file)
  */
 void close_file(int fd)
 {
-    int close_status;
-    char fd_str[10];
-    int i = 0, j;
+	int close_status;
+	char fd_str[10];
+	int i = 0, j, temp_fd;
 
-    close_status = close(fd);
+	close_status = close(fd);
 
-    if (close_status == -1)
-    {
-        write(STDERR_FILENO, "Error: Can't close fd ", 22);
+	if (close_status == -1)
+	{
+		write(STDERR_FILENO, "Error: Can't close fd ", 22);
 
-        /* Convert fd to string and write using write */
-        if (fd == 0)
-            fd_str[i++] = '0';
-        else
-        {
-            int temp_fd = fd;
-            while (temp_fd > 0)
-            {
-                fd_str[i++] = (temp_fd % 10) + '0';
-                temp_fd /= 10;
-            }
-            /* Reverse the string */
-            for (j = 0; j < i / 2; j++)
-            {
-                char temp = fd_str[j];
-                fd_str[j] = fd_str[i - j - 1];
-                fd_str[i - j - 1] = temp;
-            }
-        }
-        fd_str[i] = '\0';
+		if (fd == 0)
+			fd_str[i++] = '0';
+		else
+		{
+			temp_fd = fd;
+			while (temp_fd > 0)
+			{
+				fd_str[i++] = (temp_fd % 10) + '0';
+				temp_fd /= 10;
+			}
+			/* Reverse the string */
+			for (j = 0; j < i / 2; j++)
+			{
+				char temp = fd_str[j];
+				fd_str[j] = fd_str[i - j - 1];
+				fd_str[i - j - 1] = temp;
+			}
+		}
+		fd_str[i] = '\0';
 
-        write(STDERR_FILENO, fd_str, i);
-        write(STDERR_FILENO, "\n", 1);
-        exit(100);
-    }
+		write(STDERR_FILENO, fd_str, i);
+		write(STDERR_FILENO, "\n", 1);
+		exit(100);
+	}
 }
 
 /**
@@ -88,57 +86,57 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-    int file_from, file_to, read_status, write_status;
-    char *buffer;
-    int i;
+	int file_from, file_to, read_status, write_status;
+	char *buffer;
+	int i;
 
-    if (argc != 3)
-    {
-        write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
+		exit(97);
+	}
 
-    buffer = allocate_buffer(argv[2]);
-    file_from = open(argv[1], O_RDONLY);
-    read_status = read(file_from, buffer, BUF_SIZE);
-    file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = allocate_buffer(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	read_status = read(file_from, buffer, BUF_SIZE);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-    do {
-        if (file_from == -1 || read_status == -1)
-        {
-            write(STDERR_FILENO, "Error: Can't read from file ", 28);
-            i = 0;
-            while (argv[1][i] != '\0') /* Replacement for _strlen */
-            {
-                write(STDERR_FILENO, &argv[1][i], 1);
-                i++;
-            }
-            write(STDERR_FILENO, "\n", 1);
-            free(buffer);
-            exit(98);
-        }
+	do {
+		if (file_from == -1 || read_status == -1)
+		{
+			write(STDERR_FILENO, "Error: Can't read from file ", 28);
+			i = 0;
+			while (argv[1][i] != '\0')
+			{
+				write(STDERR_FILENO, &argv[1][i], 1);
+				i++;
+			}
+			write(STDERR_FILENO, "\n", 1);
+			free(buffer);
+			exit(98);
+		}
 
-        write_status = write(file_to, buffer, read_status);
-        if (file_to == -1 || write_status == -1)
-        {
-            write(STDERR_FILENO, "Error: Can't write to ", 22);
-            i = 0;
-            while (argv[2][i] != '\0') /* Replacement for _strlen */
-            {
-                write(STDERR_FILENO, &argv[2][i], 1);
-                i++;
-            }
-            write(STDERR_FILENO, "\n", 1);
-            free(buffer);
-            exit(99);
-        }
+		write_status = write(file_to, buffer, read_status);
+		if (file_to == -1 || write_status == -1)
+		{
+			write(STDERR_FILENO, "Error: Can't write to ", 22);
+			i = 0;
+			while (argv[2][i] != '\0')
+			{
+				write(STDERR_FILENO, &argv[2][i], 1);
+				i++;
+			}
+			write(STDERR_FILENO, "\n", 1);
+			free(buffer);
+			exit(99);
+		}
 
-        read_status = read(file_from, buffer, BUF_SIZE);
-        file_to = open(argv[2], O_WRONLY | O_APPEND);
-    } while (read_status > 0);
+		read_status = read(file_from, buffer, BUF_SIZE);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (read_status > 0);
 
-    free(buffer);
-    close_file(file_from);
-    close_file(file_to);
-    return (0);
+	free(buffer);
+	close_file(file_from);
+	close_file(file_to);
+	return (0);
 }
